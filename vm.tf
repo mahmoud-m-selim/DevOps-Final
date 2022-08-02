0819734812d7d2d1d2a9e42871c5a1006a6280ca
mahmoud-m-selim <mahmoudmselim1@gmail.com>
2022-08-02 03:10:59 +0200

    First commit

---
+++ vm.tf
@@ -0,0 +1,32 @@
+
+data "template_file" "startup_script" {
+  template = <<-EOF
+  sudo apt-get update -y
+  sudo apt-get install -y tinyproxy
+  echo "Allow localhost"| tee -a /etc/tinyproxy/tinyproxy.conf
+  systemctl restart tinyproxy
+  EOF
+}
+
+
+resource "google_compute_instance" "bastion" {
+  name         = "bastion"
+  machine_type = "e2-medium"
+  zone         = "us-west2-a"
+  tags         = ["bastionhost"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    network    = module.network.vpc_name
+    subnetwork = module.network.public_subnet_name
+
+  }
+
+  metadata_startup_script = data.template_file.startup_script.rendered
+
+}
